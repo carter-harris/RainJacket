490148fe9196fc78133f2f331705f6ad77c53f75
Hamilton Harris <hamilton.carter.harris@gmail.com>
2016-06-14 10:43:01 -0500

    initialized firebase and installed it

--- app/main.js
+++ app/main.js
@@ -1,2 +1,13 @@
-console.log("Hello World");
 angular.module('app', ['ngRoute', 'ui.bootstrap', 'angular.filter'])
+console.log("Hello World");
+
+  // Initialize Firebase
+  firebase.initializeApp ({
+    apiKey: "AIzaSyC_yMinncyZrC7PgC8ItDbDx-PZF96yFg4",
+    authDomain: "fed-capstone.firebaseapp.com",
+    databaseURL: "https://fed-capstone.firebaseio.com",
+    storageBucket: "",
+  });
+
+  // hardcoded logout, comment out / delete once we make a logout button
+  firebase.auth().signOut()
