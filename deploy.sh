62877f4ff2ccaa8de8c5d155529ba28f7c02be6c
Hamilton Harris <hamilton.carter.harris@gmail.com>
2016-09-04 20:18:49 -0500

    added bower install the the deploy script

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,7 @@
 #! /bin/sh
 git checkout -b gh-temp
 # gulp build
+# bower install
 echo '!public/lib' >> .gitignore
 git add -A
 git commit -m "lib for gh-pages"
@@ -9,3 +10,4 @@ git push origin gh-pages --force
 git checkout master
 git branch -D gh-temp
 git branch -D gh-pages
+bower install
